f56002ddeab34511c33eb9234e39fdb7946f1ab7
Gabriel-Pasquale <gabriel.prscavone@gmail.com>
2019-07-16 22:59:10 +0000

    Preparing API to Heroku

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:user) { build(:user) } 
+  let(:video) { build(:video) }
+  let(:comment) { build(:comment) }
 
+    #it { is_expected.to validate_presence_of(:user_id) }
+    #it { is_expected.to validate_presence_of(:video_id) }
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to allow_value("123456$%^&*()").for(:description) }
   
-  #pending "add some examples to (or delete) #{__FILE__}"
+  
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  #let(:user) { build(:user) } 
+  let(:user) { build(:user) } 
   let(:video) { build(:video) }
 
-  #it { is_expected.to validate_presence_of(:video) }
+  it { is_expected.to validate_presence_of(:user_id) }
   it { is_expected.to validate_presence_of(:video_text) }
   it { is_expected.to allow_value("Titulo").for(:title) }
   it { is_expected.to allow_value("123456$%^&*()").for(:video_text) }
